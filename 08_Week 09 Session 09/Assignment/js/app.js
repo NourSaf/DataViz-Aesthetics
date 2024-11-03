cc6743c99bfa033260f7554ab0942b6b475f1fed
Nour Al Safadi <nour.safadi7@icloud.com>
2024-11-03 16:49:56 -0500

    adds new function and replaces dataset

--- 08_Week 09 Session 09/Assignment/js/app.js
+++ 08_Week 09 Session 09/Assignment/js/app.js
@@ -5,7 +5,8 @@ import 'leaflet.markercluster';
 
 
 const main = d3.select('#main')
-const header =  main.append('section').attr('class','header-dev')
+const header =  main.append('section').attr('class','header-dev').text('Earthquake Map')
+
 
 const leafletContainer = main
     .append('dev')
@@ -15,17 +16,15 @@ const leafletContainer = main
 const mapContainer = leafletContainer
     .append('div')
     .attr('id','#map')
-    .style('height','500px')
-    .style('width','500px')
 
 const map = L.map(mapContainer.node(), {
-    center: [0,0],
-    zoom: 2,
+    center: [10,-10],
+    zoom: 3,
 })
 
 var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
+	minZoom: 3,
+	maxZoom: 4,
 	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	ext: 'png'
 });
@@ -68,9 +67,6 @@ console.log(cityName)
 const magArray = Array.from(mags.keys()).sort()
 console.log("this is Mag array",magArray)
 
-// const markers = latitudeArray.map((lat, i) => L.marker([lat, longitudeArray[i]]).bindPopup(cityName[i]));
-// const markerGroup = L.layerGroup(markers).addTo(map);
-
 for ( let i = 0;  i < latitudeArray.length; i++){
     console.log(latitudeArray[i],longitudeArray[i])
     if (magArray[i] >= 6){
@@ -79,7 +75,7 @@ for ( let i = 0;  i < latitudeArray.length; i++){
             weight: 0.5,
             fillColor: '#f03',
             fillOpacity: 0.5,
-            radius: 409000
+            radius: 150000
         })
             .bindPopup(cityName[i])
             .addTo(map)
@@ -92,7 +88,7 @@ for ( let i = 0;  i < latitudeArray.length; i++){
             weight: 0.5,
             fillColor: 'blue',
             fillOpacity: 0.5,
-            radius: 300000
+            radius: 150000
         })
             .bindPopup(cityName[i])
             .addTo(map)
@@ -105,28 +101,71 @@ for ( let i = 0;  i < latitudeArray.length; i++){
             weight: 0.5,
             fillColor: 'green',
             fillOpacity: 0.5,
-            radius: 200000
+            radius: 150000
         })
             .bindPopup(cityName[i])
             .addTo(map)
             .on('click', function(){
                 map.setView([latitudeArray[i], longitudeArray[i]], 6);
         });
-    }
-    
+    } 
 }
 
-const resetButton = main.append('button')
-    .attr('id', 'reset-view')
-    .text('Reset View')
-    .style('background-color', 'white')
-    .style('color', 'black')
-    .style('border', 'sloid 1px')
-    .style('padding', '10px 20px')
-    .style('cursor', 'pointer')
-    .style('border-radius', '30px')
-    .on('click', () => {
-        map.setView([0, 0], 2);
-    });
 
+L.Control.ResetButton = L.Control.extend({
+    options: {
+        position: "topleft"
+    },
+    onAdd: function (map){
+        const container = d3
+            .create('div')
+            .attr('class', "leaflet-bar leaflet-control");
 
+            const button = container
+                .append('a')
+                .attr('class','leaflet-control-button')
+                .attr('role','button')
+                .style('cursor','pointer');
+            const icon = button
+                .append('img')
+                .attr('src',"/imgs/refresh-icon.svg")
+                .style('transform','scale(0.5)');
+            button.on('click', (event) => {
+                event.preventDefault();
+                event.stopPropagation();
+                map.setView([10,-10],3)
+            });
+            return container.node();
+    },
+    onRemove:function (map){},
+});
+
+const resetView = new L.Control.ResetButton();
+resetView.addTo(map)
+
+function getColor(d) {
+    return d > 6          ? 'red'  :
+           d > 4 && d < 6 ? 'blue' :
+                            'green';
+}
+
+const legend = L.control({position:'bottomright'});
+legend.onAdd = function (map) {
+    
+    let div = L.DomUtil.create('div', 'legend');
+    let classes = [1,4,6];
+    let labels = [];
+    for (let i = 0; i < classes.length; i++){
+        div.innerHTML +=
+
+        ` 
+        <div class="legend-item"> 
+            <i style="background:${getColor(classes[i] + 1)} "></i> 
+            ${classes[i] + (classes[i + 1] ? ` &ndash; ${classes[i + 1]}<br>` : ' +')}
+        </div>
+        `
+
+    } return div;
+
+};
+legend.addTo(map);
