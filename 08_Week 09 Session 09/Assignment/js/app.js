bcc9d5e31396e458c32ed12cb4cd23e01890f4f5
Nour Al Safadi <nour.safadi7@icloud.com>
2024-11-04 19:10:53 -0500

    adds a new div with Map information

--- 08_Week 09 Session 09/Assignment/js/app.js
+++ 08_Week 09 Session 09/Assignment/js/app.js
@@ -1,11 +1,37 @@
 import * as d3 from "d3";
 import 'leaflet';
-import { latLng } from "leaflet";
 import 'leaflet.markercluster';
 
 
 const main = d3.select('#main')
-const header =  main.append('section').attr('class','header-dev').text('Earthquake Map')
+const header =  main.append('section').attr('class','header-dev')
+
+header
+    .append('div')
+    .attr('class','map-title')
+    .style('font-size', '2em')
+    .style('color', 'white')
+    .text('World Earthquakes Map')
+
+header
+    .append('p')
+    .attr('class','subtext')
+    .style('font-size', '0.9em')
+    .style('color', 'white')
+    .text('Earthquakes from 04.10 - 04.11.2024')
+
+header
+    .append('div')
+    .attr('class', 'data-source')
+    .style('font-size', '0.8em')
+    .style('color', 'gray')
+    .append('a')
+    .attr('href', 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php')
+    .attr('target', '_blank')
+    .style('text-decoration', 'none')
+    .style('color', 'gray')
+    .text('data source here');
+
 
 
 const leafletContainer = main
@@ -15,21 +41,21 @@ const leafletContainer = main
 
 const mapContainer = leafletContainer
     .append('div')
-    .attr('id','#map')
+    .attr('id','map')
 
 const map = L.map(mapContainer.node(), {
     center: [10,-10],
     zoom: 3,
 })
 
-var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+var Stadia_AlidadeSmoothDark =  L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
 	minZoom: 3,
 	maxZoom: 4,
 	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	ext: 'png'
 });
 
-const tile = map.addLayer(Stadia_AlidadeSmoothDark)
+map.addLayer(Stadia_AlidadeSmoothDark)
 
 const data = await d3.csv('data/significant_month.csv')
 console.log(data)
@@ -69,44 +95,46 @@ console.log("this is Mag array",magArray)
 
 for ( let i = 0;  i < latitudeArray.length; i++){
     console.log(latitudeArray[i],longitudeArray[i])
+    const leafletCoords = L.latLng(latitudeArray[i], longitudeArray[i]); 
     if (magArray[i] >= 6){
-        L.circle([latitudeArray[i], longitudeArray[i]], {
+        L.circle(leafletCoords, {
             color: 'red',
             weight: 0.5,
             fillColor: '#f03',
             fillOpacity: 0.5,
-            radius: 150000
+            radius: 120000
         })
-            .bindPopup(cityName[i])
+            .bindPopup(`City Name: ${cityName[i]} | Magnitude: ${magArray[i]}`)
             .addTo(map)
             .on('click', function(){
-                map.setView([latitudeArray[i], longitudeArray[i]], 6);
-        });
+                map.setView(leafletCoords, 6);
+            });
     } if (magArray[i] > 4 && magArray[i] < 6 ){
-        L.circle([latitudeArray[i], longitudeArray[i]], {
+        L.circle(leafletCoords, {
             color: 'blue',
             weight: 0.5,
             fillColor: 'blue',
             fillOpacity: 0.5,
-            radius: 150000
+            radius: 120000
         })
-            .bindPopup(cityName[i])
+            .bindPopup(`City Name: ${cityName[i]} | Magnitude: ${magArray[i]}`)
             .addTo(map)
             .on('click', function(){
-                map.setView([latitudeArray[i], longitudeArray[i]], 6);
-        });
+                map.setView(leafletCoords, 6);
+            });
     } if (magArray[i] < 4 ){
-        L.circle([latitudeArray[i], longitudeArray[i]], {
+        L.circle(leafletCoords, {
             color: 'green',
             weight: 0.5,
             fillColor: 'green',
             fillOpacity: 0.5,
-            radius: 150000
+            radius: 120000
         })
-            .bindPopup(cityName[i])
+            .bindPopup(`City Name: ${cityName[i]} | Magnitude: ${magArray[i]}`)
+           
             .addTo(map)
             .on('click', function(){
-                map.setView([latitudeArray[i], longitudeArray[i]], 6);
+                map.setView(leafletCoords, 6);
         });
     } 
 }
@@ -126,7 +154,7 @@ L.Control.ResetButton = L.Control.extend({
                 .attr('class','leaflet-control-button')
                 .attr('role','button')
                 .style('cursor','pointer');
-            const icon = button
+            button
                 .append('img')
                 .attr('src',"/imgs/refresh-icon.svg")
                 .style('transform','scale(0.5)');
@@ -137,11 +165,12 @@ L.Control.ResetButton = L.Control.extend({
             });
             return container.node();
     },
-    onRemove:function (map){},
+    onRemove:function (){},
 });
 
 const resetView = new L.Control.ResetButton();
 resetView.addTo(map)
+
 
 function getColor(d) {
     return d > 6          ? 'red'  :
@@ -149,23 +178,27 @@ function getColor(d) {
                             'green';
 }
 
-const legend = L.control({position:'bottomright'});
-legend.onAdd = function (map) {
+const legend = L.control({position:'bottomleft'});
+legend.onAdd = function () {
     
     let div = L.DomUtil.create('div', 'legend');
-    let classes = [1,4,6];
-    let labels = [];
-    for (let i = 0; i < classes.length; i++){
-        div.innerHTML +=
+    let classes = [1, 4, 6];
+    // let labels = [];
 
+    div.innerHTML += '<p class="legend-title">Earthquake Magnitude</p>';
+
+    for (let i = 0; i < classes.length; i++) {
+        div.innerHTML +=
         ` 
         <div class="legend-item"> 
             <i style="background:${getColor(classes[i] + 1)} "></i> 
             ${classes[i] + (classes[i + 1] ? ` &ndash; ${classes[i + 1]}<br>` : ' +')}
         </div>
-        `
+        `;
+    }
 
-    } return div;
-
+    return div;
 };
 legend.addTo(map);
+
+
