da813714c993ff54928e7e66c531bb136ce6fefc
Nour Al Safadi <nour.safadi7@icloud.com>
2024-11-16 22:54:00 -0500

    new

--- 11_Final-project/js/data-processing.js
+++ 11_Final-project/js/data-processing.js
@@ -1,6 +1,13 @@
 import * as d3 from "d3";
 
-const data = await d3.csv('/data/data.csv')
+const data = await d3.csv('/data/sentimentdataset.csv')
 console.log(data)
 
+const filteredData = data.filter(d => [2020, 2021, 2022, 2023].includes(+d.Year));
 
+const data_1 = d3.rollup(filteredData,
+    (v) => v.length,
+    (d) => d.Year,
+    (e) => e.Sentiment,
+);
+console.log(data_1)
