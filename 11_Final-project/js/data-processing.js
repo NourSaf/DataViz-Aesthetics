23e6d7a41f062db1f93a1e486a833fb1bfe49aed
Nour Al Safadi <nour.safadi7@icloud.com>
2024-11-17 16:36:55 -0500

    data procssing

--- 11_Final-project/js/data-processing.js
+++ 11_Final-project/js/data-processing.js
@@ -1,14 +1,35 @@
 import * as d3 from "d3";
 
-const data = await d3.csv('/data/sentimentdataset.csv')
+const data = await d3.csv('/data/data.csv')
 console.log(data)
 
-const filteredData = data.filter(d => [2020, 2021, 2022, 2023].includes(+d.Year));
 
-const data_1 = d3.rollup(filteredData,
-    (v) => v.length,
-    (d) => d.Year,
-    (e) => e.Sentiment,
+//first chart is stacked barchart 
+//sum ethnicity to have a max value for each ethnicity. //the total height of each bar.
+const sum_by_ethnicity = d3.rollup(data,
+    (v) => d3.sum(v, d => +d.complainant_age_incident),
+    (d) => d.complainant_ethnicity
 );
-console.log(data_1)
+console.log("Ethnicity sum",sum_by_ethnicity)
 
+//echnicity and age to ages accross each ethnicity 
+const ethnicity_age = d3.rollup(data,
+    (v) => v.length,
+    (d) => d.complainant_ethnicity,
+    (e) => e.complainant_age_incident,
+);
+console.log("This is Ethnictiy age -->",ethnicity_age)
+ 
+//Array.from -> converts to array from an intrable
+//goal to have an object 
+const data_object =  Array.from(ethnicity_age,([ethnicity,age]) => ({
+ethnicity: ethnicity, 
+    //converting the elemnts in the d3 rollup array of arrays map to object to easly work with 
+    ...Object.fromEntries(age)
+}))
+console.log("This is chart's one object",data_object)
+
+
+
+
+//second chart
