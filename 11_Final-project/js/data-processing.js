9584c3fccd0e09841e1a9f9ed5b2108e1c48877d
Nour Al Safadi <nour.safadi7@icloud.com>
2024-11-24 14:24:47 -0500

    adds a legend

--- 11_Final-project/js/data-processing.js
+++ 11_Final-project/js/data-processing.js
@@ -12,17 +12,36 @@ const MARGINS = {
 const data = await d3.csv('/data/data.csv')
 //console.log("This is my data",data)
 
-//first chart is stacked barchart each bar should show ethnicity general counts and euch gender stacked within.
-//echnicity and gender accross each ethnicity 
-const ethnicity_gender = d3.rollup(data,
+
+//------------------------------------------
+// FIRST CHART DATA PROCESSING
+//------------------------------------------
+
+/* 
+The first chart is stacked barchart each bar should show ethnicity general counts and euch gender stacked within echnicity and gender accross each ethnicity 
+first we have and we have mising values in the ethnicity we need to filter them out as well in the gender;
+*/
+
+function condition (d) {
+    return d.complainant_ethnicity !== "" && d.complainant_gender !== "";
+}
+
+const filteredData_chartOne = data.filter(condition)
+console.log(" This is filteredData_chartOne", filteredData_chartOne)
+
+const ethnicity_gender = d3.rollup(filteredData_chartOne,
     (v) => v.length,
     (d) => d.complainant_ethnicity,
-    (e) => e.complainant_gender,
+    (e) => e.complainant_gender
 );
-console.log("This is Ethnictiy/Gender ---->", ethnicity_gender)
+// console.log("This is Ethnictiy/Gender Map: ", ethnicity_gender)
 
-//Array.from -> converts to array from an the d3 map 
-//goal to have an object I can use for the chart
+/* 
+/Converting the d3 InternMap to array of objects 
+to have keys: ethnicty and values the gender
+goal to have an array of objects
+*/
+
 const chart_one_processed =  Array.from(ethnicity_gender,([ethnicity, gender]) => (
         {
         ethnicity: ethnicity, 
@@ -30,10 +49,8 @@ const chart_one_processed =  Array.from(ethnicity_gender,([ethnicity, gender]) =
         }
     )
 )
-console.log("This is chart's one data structure",chart_one_processed)
 
-const chartOne_dataObject  = chart_one_processed
-    .filter(d => d.ethnicity !== '')
+const chartOneData  = chart_one_processed
     .flatMap(d => 
         Object.entries(d)
             .filter(([key]) => key !== 'ethnicity' && (key === 'Male' || key === 'Female'))
@@ -43,46 +60,59 @@ const chartOne_dataObject  = chart_one_processed
                 total: total
     }))
 )
-console.log("this is ChartOne Data Object Values", chartOne_dataObject)
+console.log("this is ChartOne Data Object structure", chartOneData)
 
-const yValue = d => d.ethnicity;
-const xValue = d => d.total;
+//------------------------------------------
+// For stacked bar chart or steamgraphs we should use stacked. 
+// Stacked needs a map with keys and values. I will use flatRollup
+// to construct the correct data structre to use in stacked 
+//------------------------------------------
+
+//values to use on each axes 
+const xValue     = d => d.total;
+const yValue     = d => d.ethnicity;
 const colorValue = d => d.gender;
 
 const grouped = d3.flatRollup(
-    chartOne_dataObject,
+    chartOneData,
     (values) => 
         new Map(
-            values.map((d) => [colorValue(d), xValue(d)])
+            values.map((d) => [colorValue(d), xValue(d)]),
         ),
     yValue,
 )
-console.log("Grouped")
-console.log(grouped)
+console.log("This is grouped data: ",grouped)
+
 
 const stacked = d3.stack()
     .keys(grouped[0][1].keys())
     .value((d, key) => d[1].get(key))(grouped);
-console.log('This is Stacked')
-console.log(stacked)
+console.log('This is Stacked',stacked)
 
-//
+//------------------------------------------------------------------------------------
+// setting up scales, xScale, yScale and colorScale
+//------------------------------------------------------------------------------------
+
 const xScale = d3.scaleLinear()
     .domain(d3.extent(stacked.flat(2)))
     .range([MARGINS.LEFT, WIDTH - MARGINS.RIGHT])
+// console.log("Check xScale is working -> ",xScale.range())
 
 const yScale = d3.scaleBand()
-    .domain(chartOne_dataObject.map(yValue))
+    .domain(chartOneData.map(yValue))
     .range([HEIGHT - MARGINS.BOTTOM, MARGINS.TOP])
     .padding(0.2)
-console.log("yScale")
-console.log(yScale.domain())
+// console.log("Check yScale is working -> ",yScale.domain())
 
 
-const ChartOneColor = d3.scaleOrdinal()
+const colorScale = d3.scaleOrdinal()
     .domain(stacked.map(d => d.key))
-    .range(["#ffeda0","#feb24c","#f03b20"]);
-console.log("Colors Check", ChartOneColor.range())
+    .range(["#6e40aa","#4c6edb","#23abd8","#1ddfa3","#52f667","#aff05b"]);
+// console.log("Check if ColorScale is working", colorScale.domain())
+
+//------------------------------------------------------------------------------------
+// creating the bar using the processed data and scales ------
+//------------------------------------------------------------------------------------
 
 const chartOneContainer = d3.select('#ChartOne')
     .append('svg')
@@ -90,25 +120,59 @@ const chartOneContainer = d3.select('#ChartOne')
     .attr('height', HEIGHT)
     .attr('viewBox', [0, 0, WIDTH, HEIGHT])
 
-// Append the horizontal axis.
+// Append the horizontal axis
 chartOneContainer.append("g")
     .attr("transform", `translate(0,${HEIGHT - MARGINS.BOTTOM})`)
     .call(d3.axisBottom(xScale).tickSizeOuter(0))
 
-// Append the vertical axis.
+// Append the vertical axis
 chartOneContainer.append("g")
     .attr("transform", `translate(${MARGINS.LEFT},0)`)
     .call(d3.axisLeft(yScale).ticks(null, "s"))
 
+// chartOneContainer.selectAll(".domain").remove();
+// chartOneContainer.selectAll(".tick line").remove();
+
+//------------------------------------------------------------------------------------
+// Nesting the bars in groups for each gender a group 
+//------------------------------------------------------------------------------------
+
 chartOneContainer.append('g')
+    .attr("class", "chartOne-group")
     .selectAll('g.stacks')
     .data(stacked)
     .join('g')
-    .attr('fill', ({key}) => ChartOneColor(key))
-        .selectAll('rect')
+    .attr('class', ({key}) => key)
+    .attr('fill', ({key}) => colorScale(key))
+    .selectAll('rect')
         .data(d => d)
         .join('rect')
-        .attr('x', ([x1])=> xScale(x1))
-        .attr('y', ({data:[key]}) => yScale(key))
-            .attr('width', ([x1, x2]) => xScale(x2) - xScale(x1))
-            .attr('height', yScale.bandwidth())
+        .attr('x', ([x1]) => xScale(x1))
+        .attr('y', ({data: [key]}) => yScale(key))
+        .attr('class', ({data: [key]}) => key)
+        .attr('width', ([x1, x2]) => xScale(x2) - xScale(x1))
+        .attr('height', yScale.bandwidth())
+
+//------------------------------------------------------------------------------------
+// Adding a Legend 
+//------------------------------------------------------------------------------------
+
+const legend = chartOneContainer.append('g')
+        .attr("text-anchor", "end")
+        .selectAll('g')
+        .data(stacked.map(d => d.key))
+        .enter()
+        .append('g')
+        .attr("transform", (_, i) => `translate(0,${i * 20})`); 
+
+     legend.append('rect')
+        .attr('x', WIDTH - 19)
+        .attr("width", 19)
+        .attr("height", 19)
+        .attr("fill", colorScale)
+
+     legend.append('text')
+        .attr('fill', 'white')
+        .attr('x', WIDTH - 30)
+        .attr('y', 9.5)
+        .text(d => d)
